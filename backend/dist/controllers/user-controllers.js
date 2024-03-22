c3238709f6a884c8710e367000607001434b23e5
Anurag Kamra <anuragkamra7@gmail.com>
2024-03-22 12:01:45 +0530

    completed user login and signup

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js';
-import { hash } from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 export const getAllUsers = async (req, res, next) => {
     try {
         //get all users from db:
@@ -15,10 +15,37 @@ export const userSignup = async (req, res, next) => {
     try {
         //signing up the user:
         const { name, email, password } = req.body;
+        //check if already registered:
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send('User already registered!');
+        //encrypt the password for safety purposes:
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
+        //save and return:
         await user.save();
         return res.status(201).json({ message: 'OK', id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: 'ERROR', cause: error.message });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    try {
+        //logging in the user:
+        const { email, password } = req.body;
+        //check if not registered already:
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send('User not registered!');
+        }
+        //check if password matches with encrypted pswd:
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send('Incorrect Password!');
+        }
+        return res.status(200).json({ message: 'OK', id: user._id.toString() });
     }
     catch (error) {
         console.log(error);
--- backend/dist/utils/validators.js
+++ backend/dist/utils/validators.js
@@ -14,12 +14,15 @@ export const validate = (validations) => {
         return res.status(422).json({ errors: errors.array() });
     };
 };
-export const signupValidator = [
-    body('name').notEmpty().withMessage('Name is required'),
+export const loginValidator = [
     body('email').trim().isEmail().withMessage('Email is required'),
     body('password')
         .trim()
         .isLength({ min: 6 })
         .withMessage('Password should contain at least 6 characters.'),
+];
+export const signupValidator = [
+    body('name').notEmpty().withMessage('Name is required'),
+    ...loginValidator,
 ];
 //# sourceMappingURL=validators.js.map

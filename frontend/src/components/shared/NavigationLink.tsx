500b8a591a4ba15801325302826eb9fd8dd2d72c
Anurag Kamra <anuragkamra7@gmail.com>
2024-03-23 17:59:13 +0530

    before login start

--- frontend/src/components/shared/NavigationLink.tsx
+++ frontend/src/components/shared/NavigationLink.tsx
@@ -11,6 +11,7 @@ type Props = {
 const NavigationLink = (props: Props) => {
   return (
     <Link
+      className='nav-link'
       to={props.to}
       style={{ background: props.bg, color: props.textColor }}
     >

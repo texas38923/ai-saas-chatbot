cbe3f50f0512802d0521c066e23e568066dff340
Anurag Kamra <anuragkamra7@gmail.com>
2024-03-24 22:29:20 +0530

    completed chatCompletion functionality in backend, using openai api

--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+import { verifyToken } from '../utils/token-manager.js';
+import { chatCompletionValidator, validate } from '../utils/validators.js';
+import { generateChatCompletion } from '../controllers/chat-controllers.js';
 
+//protected api => only authenticated users can use:
 const chatRoutes = Router();
+chatRoutes.post(
+  '/new',
+  validate(chatCompletionValidator),
+  verifyToken,
+  generateChatCompletion
+);
 
 export default chatRoutes;

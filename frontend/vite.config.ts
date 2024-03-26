aff64bda928d82fbb4afadfe518dd314190237de
Anurag Kamra <anuragkamra7@gmail.com>
2024-03-26 13:57:31 +0530

    changed vite.config.ts to resolve bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    disabled: false,
+    // NOTE: workaround,  react-cropper is my nest deps
+    include: ['dynamic-import-pkg > react-cropper'],
+  },
   plugins: [react()],
-})
+  build: {
+    // Avoid @rollup/plugin-commonjs
+    commonjsOptions: {
+      include: [],
+    },
+  },
+});
